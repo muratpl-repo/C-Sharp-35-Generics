f2f1ba7df0a454ce784457cc3054dc5986bfad7c
muratpala007 <muratpala007@gmail.com>
2022-04-19 18:43:46 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _83DersGenerics //Bi metota gonderilen degerlerin turlerini genellestirmeye yarar..
+{
+    class Program
+    {
+        static void YazdirGeneric<T>(T deger) //Burada dönus tiplerini T olarak genelestirdik ve kod fazlaligindan kurtulduk...
+        {
+            Console.WriteLine(deger);
+        }
+
+        static void Main(string[] args)
+        {
+            YazdirGeneric<int>(5);
+            YazdirGeneric<double>(5.4);
+            YazdirGeneric<string>("Kaan Academy");
+            YazdirGeneric<bool>(false);
+        }
+    }
+}
